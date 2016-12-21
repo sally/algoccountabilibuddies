f403359612c422a6c560d04997060dbac95d37c7
Young Ju Park <parkyngj@gmail.com>
2016-12-21 11:42:43 -0800

    adds solution and driver code for first unique char

--- december-2016/20161221-first_unique_character_in_a_string.rb
+++ december-2016/20161221-first_unique_character_in_a_string.rb
@@ -18,3 +18,29 @@
 # Make histogram hash of occurrences for string
 # If adding to occurrences makes the value exceed 1, then delete it from the hash
 # At the end, return the first key of the hash
+
+def first_unique_character(str)
+  histogram = Hash.new(0)
+
+  str.each_char do |char|
+    if histogram[char].zero?
+      histogram[char] += 1
+    else
+      histogram.delete(char)
+    end
+  end
+
+  if histogram.keys.first
+    return str.index(histogram.keys.first)
+  else
+    return -1
+  end
+end
+
+# Driver code:
+
+s1 = "leetcode"
+p first_unique_character(s1)
+
+s2 = "loveleetcode"
+p first_unique_character(s2)
