ba3734e2847a1d6bf3b82d2f5522e2c38e2e84a4
Young Ju Park <parkyngj@gmail.com>
2016-12-21 11:42:10 -0800

    adds problem statement for first unique char

---
+++ december-2016/20161221-first_unique_character_in_a_string.rb
@@ -0,0 +1,15 @@
+# Given a string, find the first non-repeating character in it and return its index. If it doesn't, return -1.
+#
+# **Examples:**
+#
+# ```
+# s = "leetcode"
+# return 0.
+#
+# s = "loveleetcode"
+# return 2.
+# ```
+#
+# **Note:** You may assume the string contains only lowercase letters.
+
+################
