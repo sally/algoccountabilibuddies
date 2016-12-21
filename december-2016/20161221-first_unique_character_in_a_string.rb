5c4d25fd111887e5e131b3467d7c81881d8328ee
Jeffrey <jfflee.22@gmail.com>
2016-12-21 12:26:25 -0800

    refactor first solution to delete within first iteration of array

--- december-2016/20161221-first_unique_character_in_a_string.rb
+++ december-2016/20161221-first_unique_character_in_a_string.rb
@@ -12,29 +12,37 @@
 # if no count == 1 return -1
 
 def first_uniq_char(string)
-  string_array = string.chars
   # make a hash with default value of 0
   char_hash = Hash.new(0)
-  string_array.each do |char|
-    char_hash[char] += 1
+  string.chars.each do |char|
+    if char_hash[char].zero?
+      char_hash[char] += 1
+    else
+      char_hash.delete(char)
+    end
   end
 
-  char_hash.delete_if{ |char, ct| ct > 1 }
-  if char_hash.empty?
-    p -1
+
+  if char_hash.keys.first
+    char_hash.keys.first
   else
-    p char_hash.first[0]
+    -1
   end
 
 end
 
 # vroom vroom
-first_uniq_char("loveleetcode")
+puts first_uniq_char("loveleetcode")
 # should return "v"
-first_uniq_char("bobo")
+puts first_uniq_char("bobo")
 # should return -1
 
 
 # Big O Analysis
 # Time - at least O(n) because we have to iterate through the string array once. Returning values from hash is just O(1)
 # Space - O(n) because we are storing every character in a hash
+
+
+def better_return_first_uniq
+
+end
