f5ab9a64061d731768ed764edbb672d312b76bac
Young Ju Park <parkyngj@gmail.com>
2016-12-22 22:31:34 -0800

    adds kangaroo problem statement

---
+++ december-2016/20161223-kangaroos.rb
@@ -0,0 +1,24 @@
+#
+# Two kangaroos are on an x-axis traveling in a positive direction. (i.e, toward positive infinity)
+# Kangaroo 1 starts hopping at position x1 @ v1 meter/jump and kangaroo starts at position x2 @ v2 meters/jump
+#
+# Return true if they will meet, and return false if otherwise
+#
+#
+# ## Input Format
+# ```
+# An array of 4 integers denoting the respective values of x1, v1, x2, v2
+# [x1, v1, x2, v2]
+# ```
+# ## Constraints
+# ```
+# 0 <= x1 <= x2 <= 10000
+# 0 <= v1 <= 10000
+# 0 <= v2 <= 10000
+# ```
+# ## Example
+# ```
+# kangaroos_meet([43, 2, 70, 2]) will return false
+# kangaroos_meet([0, 3, 4, 2]) will return true
+# kangaroos_meet([112, 9563, 8625, 244]) will return false
+# ```
