f0abf6d2ba9ee87e34453f81c321ec8d051840e1
Young Ju Park <parkyngj@gmail.com>
2016-12-24 08:47:07 -0800

    adds problem statement of swap nodes in pairs to file

---
+++ december-2016/swap_nodes_in_pairs.rb
@@ -0,0 +1,20 @@
+# Given a linked list, swap every two adjacent nodes and return its head.
+#
+# For example,
+# Given `1->2->3->4`, you should return the list as `2->1->4->3`.
+#
+# Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
+def swap_nodes_in_pairs(head)
+  
+end
