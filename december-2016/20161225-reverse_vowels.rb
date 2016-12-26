1abb9922e642c7cdabffb8952f7756b2ac5cd35d
Young Ju Park <parkyngj@gmail.com>
2016-12-26 11:05:12 -0600

    adds pseudocode for reverse vowels

--- december-2016/20161225-reverse_vowels.rb
+++ december-2016/20161225-reverse_vowels.rb
@@ -8,3 +8,11 @@
 #
 # Note:
 # The vowels does not include the letter "y".
+
+# pseudocode
+# have two pointers, one starting at the beginning of the string, one at the end
+# until the two pointers are pointing at the same index,
+  # start iterating forwards through the string until hit a vowel
+    # when hit vowel, start iterating backwards through the string until also hit a vowel
+
+    # if this happens, then swap the two indices and continue advancing the first pointer
