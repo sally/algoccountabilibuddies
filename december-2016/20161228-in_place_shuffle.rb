e5c547b73e74c16e3e415cdae0f5e903146d4c38
Young Ju Park <parkyngj@gmail.com>
2016-12-30 18:45:49 -0600

    add pseudocode to in place shuffle

--- december-2016/20161228-in_place_shuffle.rb
+++ december-2016/20161228-in_place_shuffle.rb
@@ -3,3 +3,9 @@
 # The shuffle must be "uniform," meaning each item in the original array must have the same probability of ending up in each spot in the final array.
 #
 # Assume that you have a function `get_random(floor, ceiling)` for getting a random integer that is >= floor and <= ceiling.
+
+# pseudocode
+
+# iterate over array to shuffle:
+# for each iteratee, calculate x = get_random(iteratee_index, length - 1)
+  # swap the iteratee with item at index x
