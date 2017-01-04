ba318807de5a961c40a3908b806e314c05588d6e
Christopher Chan <christopherchan35@gmail.com>
2017-01-04 12:11:16 -0800

    leaf me be

--- december-2016/20161222-sum_of_left_leaves.rb
+++ december-2016/20161222-sum_of_left_leaves.rb
@@ -18,11 +18,25 @@ class BinaryTreeNode
   end
 end
 
+def is_a_leaf(node)
+  if !node
+    false
+  elsif !node.left && !node.right
+    true
+  end
+end
+
 def sum_of_left_leaves(root)
   sum = 0
-  if root.left != nil
-    
+  if root
+    if is_a_leaf(root.left)
+      sum += root.left.value
+    else
+      sum += sum_of_left_leaves(root.left)
+    end
+    sum += sum_of_left_leaves(root.right)
   end
+  sum
 end
 
 ###########################
@@ -36,3 +50,14 @@ twenty.insert_left(15)
 twenty.insert_right(7)
 
 puts sum_of_left_leaves(root0)
+
+root1 = BinaryTreeNode.new(5)
+root1.insert_left(3)
+eight = root1.insert_right(8)
+seven = eight.insert_left(7)
+twelve = eight.insert_right(12)
+ten = twelve.insert_left(10)
+ten.insert_left(9)
+ten.insert_right(11)
+
+puts sum_of_left_leaves(root1)
