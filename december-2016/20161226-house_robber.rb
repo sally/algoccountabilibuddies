da1d9d003fac0f35051d5db2ebf3d76d09b8c7aa
Christopher Chan <christopherchan35@gmail.com>
2017-01-05 14:15:24 -0800

    yeh no

---
+++ december-2016/20161226-house_robber.rb
@@ -0,0 +1,13 @@
+def rob_dem_houses(ary)
+  # pseudocode
+  # simplistic solution could be sum up even and odds.
+  # looking through google, it looks like a common solution is to have 2 variables, and constantly add to their sums, then take the max of the 2. eventually taking the max of the final 2 sums
+end
+
+
+###########################
+##  ｄｒｉｖｅｒ　ｃｏｄｅ  ##
+###########################
+
+puts rob_dem_houses([1,3,11,7,8,1])
+# should output 20
