bca5162a7a75be92a9be00a7a635f50354f2f07b
Young Ju Park <parkyngj@gmail.com>
2017-01-05 14:02:58 -0800

    adds solution for reverse vowels

--- december-2016/20161225-reverse_vowels.rb
+++ december-2016/20161225-reverse_vowels.rb
@@ -20,3 +20,32 @@
 # big O analysis
   # time complexity O(n)
   # space complexity O(1)
+
+def reverse_vowels(str)
+  vowels = %w(a e i o u)
+  forwards_pointer = 0
+  backwards_pointer = str.length - 1
+
+  while forwards_pointer < backwards_pointer
+    if !vowels.include?(str[forwards_pointer])
+      forwards_pointer += 1
+      next
+    end
+
+    if !vowels.include?(str[backwards_pointer])
+      backwards_pointer -= 1
+      next
+    end
+
+    preceding_vowel = str[forwards_pointer]
+    str[forwards_pointer] = str[backwards_pointer]
+    str[backwards_pointer] = preceding_vowel
+
+    forwards_pointer += 1
+    backwards_pointer -= 1
+  end
+
+  str
+end
+
+p reverse_vowels("hello")
