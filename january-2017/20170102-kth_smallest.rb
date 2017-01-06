5ff4bc199b0faa49b47fcf8a9293374d8a98715e
Young Ju Park <parkyngj@gmail.com>
2017-01-06 10:44:54 -0800

    adds extract min method for min heap

--- january-2017/20170102-kth_smallest.rb
+++ january-2017/20170102-kth_smallest.rb
@@ -10,23 +10,55 @@
 # extract min from min-heapified array k-1 times
 # get min from min-heapified array and return it
 
-def insert_into_min_heap(min_heap, element)
-  min_heap << element
-  current_index = min_heap.length
+def insert_into_min_heap(heap, element)
+  heap << element
+  current_index = heap.length
 
-  while current_index > 1 && element < min_heap[(current_index / 2) - 1]
-    larger_element = min_heap[(current_index / 2) - 1]
+  while current_index > 1 && element < heap[(current_index / 2) - 1]
+    larger_element = heap[(current_index / 2) - 1]
 
-    min_heap[(current_index / 2) - 1] = element
-    min_heap[current_index - 1] = larger_element
+    heap[(current_index / 2) - 1] = element
+    heap[current_index - 1] = larger_element
 
     current_index = (current_index / 2)
   end
 
-  min_heap
+  heap
 end
 
+def extract_min(heap)
+  min_to_return = heap.shift
 
+  heap.unshift(heap.pop)
+
+  current_index = 1
+
+  while !(heap[current_index*2 - 1].nil? && heap[current_index*2].nil?)
+    if heap[current_index*2 - 1] && heap[current_index*2]
+      if heap[current_index - 1] > heap[current_index*2 - 1] && heap[current_index*2 - 1] < heap[current_index*2]
+        index_to_swap_with = current_index*2 - 1
+      elsif heap[current_index - 1] > heap[current_index*2] && heap[current_index*2 - 1] > heap[current_index*2]
+        index_to_swap_with = current_index*2
+      else
+        break
+      end
+    elsif heap[current_index*2 - 1] && !heap[current_index*2] && heap[current_index - 1] > heap[current_index*2 - 1]
+      index_to_swap_with = current_index*2 - 1
+    elsif !heap[current_index*2 - 1] && heap[current_index*2] && heap[current_index - 1] > heap[current_index*2]
+      index_to_swap_with = current_index*2
+    else
+      break
+    end
+
+    smaller_element = heap[index_to_swap_with]
+    heap[index_to_swap_with] = heap[current_index - 1]
+    heap[current_index - 1] = smaller_element
+
+    current_index = index_to_swap_with + 1
+  end
+
+  min_to_return
+end
 
 def get_kth_smallest(array, k)
   min_heapified_array = []
@@ -37,3 +69,9 @@ def get_kth_smallest(array, k)
 end
 
 p insert_into_min_heap([15, 30, 40], 10)
+
+min_heap = [1,5,6,13,12,11]
+
+p insert_into_min_heap(min_heap, 2)
+
+p extract_min(min_heap)
