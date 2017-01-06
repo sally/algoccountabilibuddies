87741bdcef2f11e2ec86a9ede89e5f3eef07dda3
Young Ju Park <parkyngj@gmail.com>
2017-01-05 21:53:12 -0800

    add insert into min heap method

--- january-2017/20170102-kth_smallest.rb
+++ january-2017/20170102-kth_smallest.rb
@@ -3,9 +3,35 @@
 # Find the kth smallest number in an array of size `N` with `O(N)` time complexity.
 
 # pseudocode:
-# initialize max-heapified-array
+# initialize min-heapified-array
 # iterate through array:
 # for each value, say x:
-  # insert it into the max-heapified array according to rules for inserting into a max heap (percolate up)
-# extract max from max-heapified array k-1 times
-# get max from max-heapified array and return it
+  # insert it into the min-heapified array according to rules for inserting into a min heap (percolate up)
+# extract min from min-heapified array k-1 times
+# get min from min-heapified array and return it
+
+def insert_into_min_heap(min_heap, element)
+  min_heap << element
+  current_index = min_heap.length
+
+  while current_index > 1 && element < min_heap[[(current_index / 2) - 1, 0].max]
+    larger_element = min_heap[(current_index / 2) - 1]
+
+    min_heap[(current_index / 2) - 1] = element
+    min_heap[current_index - 1] = larger_element
+
+    current_index = (current_index / 2)
+  end
+
+  min_heap
+end
+
+def get_kth_smallest(array, k)
+  min_heapified_array = []
+
+  array.each do |number|
+    insert_into_min_heap(min_heapified_array, number)
+  end
+end
+
+p insert_into_min_heap([15, 30, 40], 10)
