cb13549b856bdb0b14d35d616d3b49451a880423
Young Ju Park <parkyngj@gmail.com>
2017-01-07 09:35:03 -0800

    adds problem statement for linked list palindrome

---
+++ january-2017/20170106-linked_list_palindrome.rb
@@ -0,0 +1,7 @@
+# Given a singly linked list of integers, Your task is to complete a function that returns true if the given list is palindrome, else returns false.
+#
+# Input:
+# The first line of input contains an integer T denoting the no of test cases. Then T testcases follow. Each test case contains 2 line the first line contains an integer N denoting the size of the linked list. In the next line are N space separated values of the nodes of the linked list.
+#
+# Output:
+# For each test case output will be 1 if the linked list is a pallindrome else 0.
