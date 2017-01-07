7ca910c83f51a40247e6506c1894728a38a32589
Young Ju Park <parkyngj@gmail.com>
2017-01-07 10:29:49 -0800

    adds tentative solution for linked list palindrome

--- january-2017/20170106-linked_list_palindrome.rb
+++ january-2017/20170106-linked_list_palindrome.rb
@@ -5,3 +5,50 @@
 #
 # Output:
 # For each test case output will be 1 if the linked list is a pallindrome else 0.
+
+# pseudocode:
+# initialize a stack to keep track of the first floor(n/2) elements in the linked list, where n is the given length
+# depending on whether n is odd or even:
+  # if n is odd, then let skip over the element in position floor(n/2) + 1
+# continue iterating over the last floor(n/2) elements, popping off of the stack as we check for equality
+  # if ever equality is not met, then return false
+# if iteration is complete (and stack is empty?) return true
+
+# Ruby Linked List Node Implementation:
+
+class LinkedListNode
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+
+end
+
+def linked_list_palindrome(n, head)
+  node_stack = []
+
+  current_node = head
+
+  for k in (0..n/2) do
+    node_stack << current_node.value
+    current_node = current_node.next
+  end
+
+  if n.odd?
+    current_node = current_node.next
+  end
+
+  for k in (n/2+1...n) do
+    if node_stack.last == current_node.value
+      node_stack.pop
+      current_node = current_node.next
+    else
+      false
+    end
+  end
+
+  true
+end
