7dd449236fae0c5ead44f8f5dbe7dd3ef43ab242
Young Ju Park <parkyngj@gmail.com>
2017-01-06 20:15:27 -0800

    adds problem statement to connected cells

---
+++ january-2017/20170103-connected_cells_in_grid.rb
@@ -0,0 +1,1 @@
+# Given an `n*m` matrix, find and print the number of cells in the largest region in the matrix, where region is defined as given in the source link. Note that there may be more than one region in the matrix.
