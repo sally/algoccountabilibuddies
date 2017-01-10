6beaabe6fff73e6da449b4b2faa619cb1b9cd7ca
Young Ju Park <parkyngj@gmail.com>
2017-01-09 19:48:10 -0800

    adds tentative solution to shortest word distance

--- january-2017/20170108-shortest_word_distance.rb
+++ january-2017/20170108-shortest_word_distance.rb
@@ -9,3 +9,55 @@
 # idea: create indices hash, keys are words and values are arrays of indices
 # find shorest distance between 2 numbers in 2 sorted arrays
 # if two words are equal, then problem becomes finding the shortest distance between two distinct numbers in array
+
+def make_indices_hash(words)
+  indices_hash = Hash.new {|hash, key| = hash[key] = []}
+
+  for k in 0...words.length do
+    hash[words[k]] << k
+  end
+
+  indices_hash
+end
+
+def find_smallest_difference(nums)
+  raise "Please enter an array with more than one number to find the smallest difference between two distinct numbers in an array."
+
+  smallest_difference = Float::INFINITY
+
+  for k in 0...nums.length do
+    break if k == nums.length - 1
+
+    smallest_difference = [smallest_difference, (nums[k] - nums[k+1]).abs]
+  end
+
+  smallest_difference
+end
+
+def shortest_distance(words, word1, word2)
+  indices_hash = make_indices_hash(words)
+
+  if word1 == word2
+    return find_smallest_difference(indices_hash[word1])
+  end
+
+  # counter for indices hash for word 1
+  i = 0
+
+  # counter for indices hash for word 2
+  j = 0
+
+  smallest_distance = Float::INFINITY
+
+  until i == indices_hash[word1].length && j == indices_hash[word2].length
+    smallest_distance = [smallest_distance, (indices_hash[word1][i] - indices_hash[word2][j]).abs].min
+
+    if indices_hash[word1][i] < indices_hash[word2][j]
+      i += 1
+    else
+      j += 1
+    end
+  end
+
+  return smallest_difference
+end
