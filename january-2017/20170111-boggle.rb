c167aa83718a7db0724b8dfd06c1c59368934f2f
Young Ju Park <parkyngj@gmail.com>
2017-01-10 12:53:32 -0800

    add idea and pseudocode for boggle

--- january-2017/20170111-boggle.rb
+++ january-2017/20170111-boggle.rb
@@ -17,3 +17,15 @@
 #          GEEKS
 #          QUIZ
 # ```
+
+# idea: iterate through 2d matrix, if run into first letter of one of the dictionary words, then begin a breadth first search for the rest of the letters in that word
+  # caveats: can't backtrack (means should keep another matrix keeping track of coordinates visited for each breadth first search)
+
+# utility functions:
+  # helper function to check whether it is safe or not to add a coordinate to dfs queue (must be a valid coordinate, i.e. doesn't exceed or fall below the dimensions of the matrix)
+  # helper function to do the dfs (has directions to go in)
+
+# for each word in dictionary:
+  # iterate through boggle and look for first character
+  # if found first character:
+    # start depth first search for rest of characters
